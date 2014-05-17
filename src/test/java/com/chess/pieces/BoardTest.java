d67631953643cea0ae1c0f064c6ceaabfc6e1b96
liu liang <liul85@gmail.com>
2014-05-17 21:23:47 +0800

    add setUp in BoardTest

--- src/test/java/com/chess/pieces/BoardTest.java
+++ src/test/java/com/chess/pieces/BoardTest.java
@@ -6,15 +6,21 @@ import junit.framework.TestCase;
  * Created by MAC on 5/17/14.
  */
 public class BoardTest extends TestCase {
+
+    private Board board;
+    private Pawn pawn1, pawn2;
+
+    public void setUp() {
+        board = new Board();
+        pawn1 = Pawn.createPawn("white");
+        pawn2 = Pawn.createPawn("black");
+    }
     public void testCreate() {
-        Board board = new Board();
         assertEquals(0, board.getNumberOfPawns());
     }
 
     public void testAddPawnsToBoard() {
         Board board = new Board();
-        Pawn pawn1 = Pawn.createPawn("white");
-        Pawn pawn2 = Pawn.createPawn("black");
 
         board.enrollPawn(0, pawn1);
         board.enrollPawn(1, pawn2);
